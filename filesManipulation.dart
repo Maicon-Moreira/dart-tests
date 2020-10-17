7046ce9733221b4622b56b90855a11c463d78b6f
Maicon-Moreira <maiconmoreirapira@gmail.com>
2020-10-16 23:06:31 -0300

    filesManipulation

---
+++ filesManipulation.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  Example example = new Example();
+
+  example.read();
+
+  example.write();
+}
+
+class Example {
+  Example() {
+    print('this is the constructor !!!!!');
+  }
+
+  void read() {
+    print('-' * 100); // Python S2
+    print('Read file test');
+
+    File file = new File('file.txt');
+
+    file.readAsString().then((String contents) {
+      print(contents);
+    });
+  }
+
+  void write() {
+    print('-' * 100);
+    print('Write file test');
+
+    File file = new File('written.txt');
+
+    Random randomSeed = new Random();
+
+    int randomNumber = randomSeed.nextInt(pow(2, 32));
+
+    String fileContent = 'Random number: $randomNumber';
+
+    file.writeAsString(fileContent);
+  }
+}
